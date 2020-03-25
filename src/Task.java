e173dc4134c513dbb4a810305a22005a4962848c
kremlev404 <kremlev404@gmail.com>
2020-03-25 21:44:04 +0300

    Worker And clean App.class

--- src/Task.java
+++ src/Task.java
@@ -1,13 +1,15 @@
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 interface  Task{
     int Question( int type_);
 }
 
-class Task_class implements Task{
+class Task_class implements Task {
 
     @Override
-    public int Question( int type_) {
+    public int Question(int type_) {
 
         if (type_ == 1) {
             Scanner in = new Scanner(System.in);
@@ -41,9 +43,91 @@ class Task_class implements Task{
             int num = in.nextInt();
             in.close();
             return num;
-        }
-        else {
+        } else {
             return 0;
+        }
+    }
+}
+
+class Worker extends Task_class{
+    public static ArrayList<Integer> listInt;
+    public static String[][] listStr;
+
+    Worker(ArrayList<Integer> listInt, String[][] listStr) throws IOException, InterruptedException {
+        this.listInt = listInt;
+        this.listStr = listStr;
+    }
+
+    public static void work() throws IOException, InterruptedException{
+
+        //Create to hands ( int and string) to help with task
+        Handler int_hand = new Handler();
+        Handler str_hand = new Handler();
+
+        App task = new App();
+        int  q = 0;
+        int type = 0;
+        String Type = " ";
+        q = task.Question(1);
+        switch (q) {
+            case 1:
+                listInt = Generator.Gen(type);
+                q = task.Question(2);
+                if (q == 1) {
+                    int_hand.getIntResultFirst(listInt);
+                    break;
+                }
+                if (q == 2) {
+                    q = task.Question(3);
+                    if (q == 1 || q == 2) {
+                        int_hand.getIntResultSecond(listInt, q);
+                        break;
+                    } else {
+                        System.out.println("Ошибка ввода!");
+                        System.exit(0);
+                    }
+                }
+                if (q == 3) {
+                    q = task.Question(3);
+                    if (q == 1 || q == 2) {
+                        int_hand.getIntResultFirst(listInt);
+                        System.out.println();
+                        int_hand.getIntResultSecond(listInt, q);
+                        break;
+                    } else {
+                        System.out.println("Ошибка ввода!");
+                        System.exit(0);
+                    }
+                } else {
+                    System.out.println("Ошибка ввода!");
+                    System.exit(0);
+                }
+
+            case 2:
+                listStr = Generator.Gen(Type);
+                q = task.Question(4);
+                if (q == 1) {
+                    str_hand.getStrResultFirst(listStr);
+                    break;
+                }
+                if (q == 2) {
+                    str_hand.getStrResultSecond(listStr);
+                    break;
+                }
+                if (q == 3) {
+                    str_hand.getStrResultFirst(listStr);
+                    System.out.println();
+                    str_hand.getStrResultSecond(listStr);
+                    break;
+                } else {
+                    System.out.println("Ошибка ввода!");
+                    System.exit(0);
+                }
+
+            default:
+                System.out.println("ERROR");
+                break;
+
         }
     }
 }
